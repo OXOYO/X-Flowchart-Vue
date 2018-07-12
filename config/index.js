dbf9014f1b800a9be66b8b6f4af40bfb50c81825
OXOYO <zmn2007.hi@163.com>
2018-07-12 17:48:53 +0800

    更新打包输出目录。

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/X-Flowchart-Vue/',
 
     /**
      * Source Maps
